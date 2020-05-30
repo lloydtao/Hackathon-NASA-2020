9a793f35ba748176cb2d7b994a385694d441f9d3
LloydTao <LL23@live.co.uk>
2020-05-30 13:03:42 +0100

    Install crispy forms

--- src/website/settings.py
+++ src/website/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'bunchup',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'bunchup-home'
 
