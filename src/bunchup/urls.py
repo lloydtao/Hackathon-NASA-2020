001183323642fdbe0ecdd6a363afbcda1616ecc0
LloydTao <LL23@live.co.uk>
2020-05-30 18:24:20 +0100

    Route bunchup app to views

--- src/bunchup/urls.py
+++ src/bunchup/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, HubView, HubCreateView, ActivityView, ActivityCreateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='bunchup-home'),
+    path('hub/<str:username>', HubView.as_view(), name='bunchup-hub'),
+    path('activity/<str:username>', ActivityView.as_view(), name='bunchup-activity'),
+    path('create/hub/', HubCreateView.as_view(), name='bunchup-hub-create'),
+    path('create/activity/', ActivityCreateView.as_view(), name='bunchup-activity-create'),
 ]
