6aa9dfedfe7738ec23430d5fb5e35707a892d1e4
LloydTao <LL23@live.co.uk>
2020-05-30 18:28:47 +0100

    Register bunchup models in admin panel

--- src/bunchup/admin.py
+++ src/bunchup/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Activity)
+admin.site.register(Hub)
+admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Membership)
