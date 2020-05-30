c12c6ce378be766edbf6f32db0da415f703c66e3
LloydTao <LL23@live.co.uk>
2020-05-30 13:02:22 +0100

    Add HomeView route using Hub model

--- src/bunchup/views.py
+++ src/bunchup/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .models import Hub
 
 class HomeView(ListView):
+    model = Hub
     template_name = 'bunchup/index.html'
