5fe9f09c67c85ad2823640a74ec97c80cbbf26c6
LloydTao <LL23@live.co.uk>
2020-05-30 15:56:17 +0100

    Fix missing **kwargs in profiles

--- src/users/models.py
+++ src/users/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     
-    def save(self):
+    def save(self, **kwargs):
         super().save()
         img = Image.open(self.image.path)
         if img.height > 320 or img.width > 320:
