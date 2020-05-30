4e6506dbbc9478d3056407fee646524855175c61
LloydTao <LL23@live.co.uk>
2020-05-30 11:52:49 +0100

    Add user profiles to users app

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 320 or img.width > 320:
+            size = (300, 300)
+            img.thumbnail(size, Image.ANTIALIAS)
+            img.save(self.image.path)
