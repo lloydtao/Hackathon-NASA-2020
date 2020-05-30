207111a3487f678b6343998f914fd5c3c02b4a0b
LloydTao <LL23@live.co.uk>
2020-05-30 22:06:32 +0100

    Use 'hub/<int:pk>/edit/' as hub edit path

--- src/bunchup/urls.py
+++ src/bunchup/urls.py
@@ -4,7 +4,7 @@ from .views import HomeView, HubView, HubCreateView, HubUpdateView, ActivityView
 urlpatterns = [
     path('', HomeView.as_view(), name='bunchup-home'),
     path('hub/<int:pk>', HubView.as_view(), name='bunchup-hub'),
-    path('hub_edit/<int:pk>', HubUpdateView.as_view(), name='bunchup-hub-update'),
+    path('hub/<int:pk>/edit/', HubUpdateView.as_view(), name='bunchup-hub-update'),
     path('activity/<str:username>', ActivityView.as_view(), name='bunchup-activity'),
     path('create/hub/', HubCreateView.as_view(), name='bunchup-hub-create'),
     path('create/activity/', ActivityCreateView.as_view(), name='bunchup-activity-create'),
