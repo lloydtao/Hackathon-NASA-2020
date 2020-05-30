3b0dcabb493bc05d71e6d033b9f040e28f432389
Matt Drayton <matthewdrayton@outlook.com>
2020-05-30 19:10:34 +0100

    Fixed changing profile by removing password form

--- src/users/views.py
+++ src/users/views.py
@@ -26,22 +26,18 @@ def profile(request):
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, instance=request.user)
         profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        password_form = PasswordChangeForm(request.user, request.POST)
-        if user_form.is_valid() and profile_form.is_valid() and password_form.is_valid():
+        if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            password_form.save()
             messages.success(request, f'Your account has been updated.')
             return redirect('profile')
     else:
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.profile)
-        password_form = PasswordChangeForm(request.user)
-        
+
     context = {
         'user_form': user_form,
         'profile_form': profile_form,
-        'password_form': password_form
     }
     return render(request, 'users/profile.html', context)
 
