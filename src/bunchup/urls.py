59869dd880109c470f0c371632455879cc37121c
Matt Drayton <matthewdrayton@outlook.com>
2020-05-31 15:09:48 +0100

    Add redirect for hub creation
    Add ActivityUpdateView
    
    Add ActivityDeleteView

--- src/bunchup/urls.py
+++ src/bunchup/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('hub/<int:pk>/edit/', HubUpdateView.as_view(), name='bunchup-hub-update'),
     path('hub/<int:pk>/delete/', HubDeleteView.as_view(), name='bunchup-hub-delete'),
     path('activity/<int:pk>/', ActivityView.as_view(), name='bunchup-activity'),
+    path('activity/<int:pk>/delete', ActivityView.as_view(), name='bunchup-activity-delete'),
+    path('activity/<int:pk>/update', ActivityView.as_view(), name='bunchup-activity-update'),
     path('create/hub/', HubCreateView.as_view(), name='bunchup-hub-create'),
     path('hub/<int:pk>/create/activity/', ActivityCreateView.as_view(), name='bunchup-activity-create'),
 ]
