244a8f5e639429aba8226cb22bdfcda262f3fb02
Matt Drayton <matthewdrayton@outlook.com>
2020-05-31 18:25:21 +0100

    Add view for joining hub
    Add view for joining activity

--- src/bunchup/urls.py
+++ src/bunchup/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from .views import HomeView, HubView, HubCreateView, HubUpdateView, HubDeleteView, ActivityView, ActivityCreateView
+from .views import HomeView, HubView, HubCreateView, HubUpdateView, HubDeleteView, ActivityView, ActivityCreateView, ActivityJoinView, HubJoinView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='bunchup-home'),
     path('hub/<int:pk>/', HubView.as_view(), name='bunchup-hub'),
     path('hub/<int:pk>/edit/', HubUpdateView.as_view(), name='bunchup-hub-update'),
     path('hub/<int:pk>/delete/', HubDeleteView.as_view(), name='bunchup-hub-delete'),
+    path('hub/<int:pk>/join', HubJoinView.as_view(), name='bunchup-hub-join'),
     path('activity/<int:pk>/', ActivityView.as_view(), name='bunchup-activity'),
     path('activity/<int:pk>/delete', ActivityView.as_view(), name='bunchup-activity-delete'),
     path('activity/<int:pk>/update', ActivityView.as_view(), name='bunchup-activity-update'),
+    path('activity/<int:pk>/join', ActivityJoinView.as_view(), name='bunchup-activity-join'),
     path('create/hub/', HubCreateView.as_view(), name='bunchup-hub-create'),
     path('hub/<int:pk>/create/activity/', ActivityCreateView.as_view(), name='bunchup-activity-create'),
 ]
--- src/bunchup/views.py
+++ src/bunchup/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
 from .models import Hub, Activity, Membership, Room
 from .forms import ImageUploadForm
 
@@ -38,12 +38,13 @@ class HubCreateView(LoginRequiredMixin, CreateView):
         )
         return HttpResponseRedirect(reverse_lazy("bunchup-hub", kwargs={"pk": str(self.object.pk)}))
 
+
 class HubUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Hub
     form_class = ImageUploadForm
     context_object_name = 'hubs'
     template_name = 'bunchup/hub_update.html'
-    
+
     def test_func(self):
         hub = self.get_object()
         if hub.membership_set.filter(user=self.request.user).exists():
@@ -130,3 +131,21 @@ class ActivityUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return hub.membership_set.get(user=self.request.user).is_admin
         else:
             return False
+
+
+class ActivityJoinView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        print("PRIMARY KEY: ", kwargs['pk'])
+        activity = Activity.objects.get(pk=kwargs['pk'])
+        activity.users.add(self.request.user)
+        activity.save()
+        return HttpResponseRedirect(reverse_lazy("bunchup-activity", kwargs={"pk": str(activity.pk)}))
+
+
+class HubJoinView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        print("PRIMARY KEY: ", kwargs['pk'])
+        hub = Hub.objects.get(pk=kwargs['pk'])
+        hub.users.add(self.request.user)
+        hub.save()
+        return HttpResponseRedirect(reverse_lazy("bunchup-hub", kwargs={"pk": str(hub.pk)}))
